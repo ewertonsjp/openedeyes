3898f852ef9934b94d2d15cd6af79e8de9d1a146
Ewerton Costa <ewertonsjp@gmail.com>
2017-05-03 09:35:12 -0300

    Show graphs using Charts.js

--- src/pages/indicator/indicator.ts
+++ src/pages/indicator/indicator.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 /**
  * Generated class for the Indicator page.
@@ -13,7 +14,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IndicatorPage {
 
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  ionViewDidLoad() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+                {
+                    label: "SQLs",
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [3, 3, 3, 15, 15, 10],
+                    spanGaps: false,
+                }
+            ]
+        }
+    });
+  }
 }
